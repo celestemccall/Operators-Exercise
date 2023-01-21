e18262fa2584a2e8a57cfd7486699662f67f4671
Celeste McCall <celestemccall93@gmail.com>
2023-01-20 20:24:06 -0600

    Inital Commit

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+
+            Console.WriteLine("What is the radius of your circle");
+
+            var radius = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(AreaOfCircle(radius));
+
+
         }
+
+        public static double AreaOfCircle(double radius)
+        {
+
+            var area = Math.PI * Math.Pow(radius, 2);
+
+            return area;
+
+
+
+        }
+
+
     }
+
 }
